4f4d41e43dc9ac0d2b52692d237e689b715c14c5
mrostami0 <mohammad.rostami13@gmail.com>
2022-09-16 00:29:21 +0430

    03. Testing Basics

---
+++ 03-testing-basics/basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,24 @@
+import {transformToNumber} from './numbers';
+
+import {it, expect} from "vitest";
+
+it('should transform a string number to a number of type number', function () {
+    const input = '1';
+    const result = transformToNumber(input);
+    expect(result).toBeTypeOf('number');
+});
+
+it('should transform a string number to a number of type number', function () {
+    const input = '1';
+    const result = transformToNumber(input);
+    expect(result).toBe(+input);
+});
+
+it('should yield NaN for not-transformable values', function () {
+    const input = 'invalid';
+    const input2 = {};
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+}); 
